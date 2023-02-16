92040e2b6fad075a08fd0a81a3b94a6f550bda32
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-02-16 11:05:17 -0500

    started working on part 1, still have to finish, add if/else if/ else statements, and also error

--- Program.cs
+++ Program.cs
@@ -1,23 +1,26 @@
-﻿namespace Part_5___If_Statements
+﻿using System.Runtime.CompilerServices;
+
+namespace Part_5___If_Statements
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Random generator = new Random();
 
+            // ------------------------------------------------------------------------------- ONE
+
+            double fee, initialBalance;
+            string transactionType;
             Console.WriteLine("Simple Banking Machines");
-            int num;
-            Console.WriteLine("pick a number");
-            num = Convert.ToInt32(Console.ReadLine()); 
-
-            if(num > 5 && num <15)
-            {
-                Console.WriteLine("between");
-            }
-            else
-            {
-                Console.WriteLine("not between");
-            }
+            fee = 0.75;
+            initialBalance = generator.Next(100, 150);
+            Console.WriteLine("Bank of Blorb");
+            //Console.WriteLine($"Your bank balance is: ${initialBalance}");
+            Console.WriteLine("Transactions Available: Deposit, Withdrawl, bill payment, account balance");
+            Console.WriteLine("Pick one");
+            transactionType = Console.ReadLine().ToUpper();
+            Console.WriteLine(transactionType);
 
         }
     }
