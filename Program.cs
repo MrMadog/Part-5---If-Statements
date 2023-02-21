e952b24f303eb710f534d034fa7423f5b7ed72e4
Maddox Macnamara <tpmac001maddog@gmail.com>
2023-02-21 10:23:49 -0500

    Finished up the rest of part 5 and did some work with switch statements

--- Program.cs
+++ Program.cs
@@ -233,27 +233,46 @@ namespace Part_5___If_Statements
                 Console.WriteLine("");
                 Console.WriteLine("Hurricane Intensity Scale(Saffir-Simpson)");
 
+
                 int category;
                 Console.WriteLine("Pick a category(1-5): ");
-                category = Convert.ToInt32(Console.ReadLine());
-
-                switch (category)
+                if (Int32.TryParse(Console.ReadLine(), out category))
                 {
-                    case > 1:
-                        Console.WriteLine("Hello world");
-                        break;
+                    Console.WriteLine("");
+                    Console.WriteLine($" Category {category}: ");
+                    switch (category)
+                    {
+                        case 1:
+                            Console.WriteLine("    74-95 mph or 64-82 kt or 119-153 kmh");
+                            break;
 
-                    case < 1:
-                        Console.WriteLine("Goodbye world");
-                        break;
+                        case 2:
+                            Console.WriteLine("    96-110 mph or 83-95 kt or 154-177 kmh");
+                            break;
 
-                    default:
-                        Console.WriteLine("fgniadef");
-                        break;
+                        case 3:
+                            Console.WriteLine("    111-130 mph or 93-113 kt or 178-209 kmh");
+                            break;
+
+                        case 4:
+                            Console.WriteLine("    131-155 mph or 114-135 kt or 210-249 kmh");
+                            break;
+
+                        case 5:
+                            Console.WriteLine("    greater than 155 mph or 135 kt or 249kmh");
+                            break;
+
+                        default:
+                            Console.WriteLine("That was not an option!");
+                            break;
+                    }
                 }
 
+                else
+                {
+                    Console.WriteLine("Invalid Input.");
+                }
             }
-
         }
     }
 }
